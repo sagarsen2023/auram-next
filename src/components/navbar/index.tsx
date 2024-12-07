4e6043b1175af01aab2abc140694059e1496e239
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-07 12:13:25 +0530

    Add CartButtonComponent and integrate it into the NavBarComponent

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,16 +1,29 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
-import dynamic from "next/dynamic";
-const ThemeSwitchButtonComponent = dynamic(
-  () => import("../buttons/theme-switch-button.component"),
-  { ssr: false }
-);
+import { CiMenuFries } from "react-icons/ci";
+import auramLogoWithText from "../../../public/images/auram-logo-with-text.webp";
+import CartButtonComponent from "../buttons/cart-button.component";
+// import dynamic from "next/dynamic";
+// const ThemeSwitchButtonComponent = dynamic(
+//   () => import("../buttons/theme-switch-button.component"),
+//   { ssr: false }
+// );
 
 function NavBarComponent() {
   return (
-    <div>
-      <ThemeSwitchButtonComponent />
+    <div className="flex justify-between items-center p-2 shadow-md">
+      <Image
+        src={auramLogoWithText}
+        alt="Auram Logo with text"
+        className="w-auto h-12"
+      />
+      {/* <ThemeSwitchButtonComponent /> */}
+      <div className="flex items-center gap-4 text-2xl">
+        <CartButtonComponent />
+        <CiMenuFries />
+      </div>
     </div>
   );
 }
