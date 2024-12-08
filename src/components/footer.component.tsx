1fe3f7b2a92336c7d3962297667550eaaf762db5
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-08 14:13:51 +0530

    Enhance FooterComponent with dynamic data fetching for collections and contact information; restructure layout for improved user experience

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -1,7 +1,14 @@
+import { AuramContactInfoModel } from "@/models/contact/auram-contact-info.model";
+import { CollectionItem } from "@/models/product-category-collections/collection.model";
+import { contactAPI } from "@/services/contact.service";
+import { collectionAPI } from "@/services/product.service";
+import Link from "next/link";
 import React from "react";
-import { FaGlobe } from "react-icons/fa";
+import { FaGlobe, FaMapMarkerAlt, FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
+import { IoMailSharp } from "react-icons/io5";
 
-function FooterComponent() {
+async function FooterComponent() {
+  const sectionClassName = "flex flex-col items-center w-full";
   const testimonials = [
     {
       icon: <FaGlobe />,
@@ -24,30 +31,180 @@ function FooterComponent() {
       description: "If your are unable.",
     },
   ];
+  // TODO: Replace with actual links
+  const informationData = [
+    {
+      title: "About Us",
+      href: "/",
+    },
+    {
+      title: "Delivery Information",
+      href: "/",
+    },
+    {
+      title: "Privacy Policy",
+      href: "/",
+    },
+    {
+      title: "Terms & Conditions",
+      href: "/",
+    },
+    {
+      title: "Site Map",
+      href: "/",
+    },
+  ];
+  const extrasData = [
+    {
+      title: "Contact us",
+      href: "/",
+    },
+    {
+      title: "Returns",
+      href: "/",
+    },
+    {
+      title: "Specials",
+      href: "/",
+    },
+    {
+      title: "Brands",
+      href: "/",
+    },
+    {
+      title: "Gift Vouchers",
+      href: "/",
+    },
+    {
+      title: "Affiliates",
+      href: "/",
+    },
+  ];
+  let collectionData: CollectionItem[] = [];
+  let contactInfoData: AuramContactInfoModel = {
+    _id: "",
+    address: "",
+    whatsapp: "",
+    email: "",
+    facebook: "",
+    instagram: "",
+    linkedin: "",
+    phone: "",
+    twitter: "",
+    youtube: "",
+  };
+  try {
+    const collectionResponse = await collectionAPI.getAllCollections();
+    collectionData = collectionResponse.data;
+    const contactInfoResponse = await contactAPI.getAuramContactInfo();
+    contactInfoData = contactInfoResponse.data;
+  } catch {
+    console.error("Error fetching collections");
+  }
 
   return (
-    <div className="w-full bg-gray-100 flex justify-center">
-      <div className="w-full lg:max-w-8xl">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-0 md:gap-4">
-          {testimonials.map((testimonial, index) => (
-            <div
-              key={index}
-              className={`${
-                index === 3 && "bg-gray-200"
-              } px-4 py-5 flex items-center justify-center border-b-2 group`}
-            >
-              <div className="border-b border-secondary">
-                <FaGlobe className="text-3xl pb-1 text-secondary group-hover:-translate-y-3 duration-300" />
+    <>
+      {/* Testimonial Part */}
+      <footer className="w-full bg-gray-100 flex flex-col items-center justify-center gap-5">
+        <div className="w-full lg:max-w-8xl">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-0 md:gap-4">
+            {testimonials.map((testimonial, index) => (
+              <div
+                key={index}
+                className={`${
+                  index === 3 && "bg-gray-200"
+                } px-4 py-5 flex items-center justify-center border-b-2 group`}
+              >
+                <div className="border-b border-secondary">
+                  <FaGlobe className="text-3xl pb-1 text-secondary group-hover:-translate-y-3 duration-300" />
+                </div>
+                <div className="w-full text-center">
+                  <h2 className="font-[600]">{testimonial.title}</h2>
+                  <p className="text-sm">{testimonial.description}</p>
+                </div>
               </div>
-              <div className="w-full text-center">
-                <h2 className="font-[600]">{testimonial.title}</h2>
-                <p className="text-sm">{testimonial.description}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </div>
+        <div className="w-full lg:max-w-8xl grid grid-cols-1 md:grid-cols-4 gap-10 lg:gap-4">
+          {/* Collection Info */}
+          <div className={`${sectionClassName}`}>
+            <h1 className="pb-2 mb-2 border-b-2 border-secondary w-fit">
+              Collections
+            </h1>
+            {collectionData.map((collection) => (
+              <Link key={collection._id} href={"/"}>
+                <p className="py-1 lg:py-1.5 text-sm">{collection.title}</p>
+              </Link>
+            ))}
+          </div>
+          {/* Information */}
+          <div className={`${sectionClassName}`}>
+            <h1 className="pb-2 mb-2 border-b-2 border-secondary w-fit">
+              Information
+            </h1>
+            {informationData.map((info) => (
+              <Link key={info.title} href={info.href}>
+                <p className="py-1 lg:py-1.5 text-sm">{info.title}</p>
+              </Link>
+            ))}
+          </div>
+          {/* Extras */}
+          <div className={`${sectionClassName}`}>
+            <h1 className="pb-2 mb-2 border-b-2 border-secondary w-fit">
+              Extras
+            </h1>
+            {extrasData.map((extra) => (
+              <Link key={extra.title} href={extra.href}>
+                <p className="py-1 lg:py-1.5 text-sm">{extra.title}</p>
+              </Link>
+            ))}
+          </div>
+          {/* Contact Info */}
+          <div className={`${sectionClassName} bg-gray-200 -mt-5`}>
+            <h1 className="mt-5 pb-2 mb-2 border-b-2 border-secondary w-fit">
+              Contact Us
+            </h1>
+            {contactInfoData?.email && (
+              <div className="flex items-center gap-2">
+                <IoMailSharp className="" />
+                <Link href={`mailto:${contactInfoData.email}`}>
+                  <p className="py-1 lg:py-1.5 text-sm">
+                    {contactInfoData.email}
+                  </p>
+                </Link>
+              </div>
+            )}
+            {contactInfoData?.phone && (
+              <div className="flex items-center gap-2">
+                <FaPhoneAlt className="" />
+                <p className="py-1 lg:py-1.5 text-sm">
+                  {contactInfoData.phone}
+                </p>
+              </div>
+            )}
+            {contactInfoData?.whatsapp && (
+              <div className="flex items-center gap-2">
+                <FaWhatsapp className="" />
+                <Link href={`https://wa.me/${contactInfoData.whatsapp}`}>
+                  <p className="py-1 lg:py-1.5 text-sm">
+                    {contactInfoData.whatsapp}
+                  </p>
+                </Link>
+              </div>
+            )}
+            {contactInfoData?.address && (
+              <div className="flex items-center gap-2">
+                <FaMapMarkerAlt className="" />
+                <p className="py-1 lg:py-1.5 text-sm">
+                  {contactInfoData.address}
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
 
