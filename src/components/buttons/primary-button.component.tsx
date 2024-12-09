4f008a77243c7008342be290e0c79cdac683d511
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-10 01:16:32 +0530

    Add PrimaryButtonComponent with loading state and customizable styles

---
+++ src/components/buttons/primary-button.component.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { ReactNode } from "react";
+import classNameMerger from "@/utils/class-name-merger";
+
+function PrimaryButtonCOmponent({
+  isLoading,
+  className,
+  children,
+  onClick,
+  loaderClassName,
+}: {
+  loaderClassName?: string;
+  isLoading?: boolean;
+  className?: string;
+  children?: ReactNode;
+  onClick?: () => void;
+}) {
+  return (
+    <button
+      className={classNameMerger(
+        "bg-primary text-white w-full py-2 rounded-lg px-3 flex items-center justify-center hover:bg-gray-800 hover:shadow-lg disabled:opacity-50 transition-opacity duration-250",
+        className
+      )}
+      onClick={onClick}
+      disabled={isLoading}
+    >
+      {isLoading && (
+        <svg
+          className={classNameMerger(
+            "animate-spin h-5 w-5 mr-3",
+            loaderClassName
+          )}
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+            fill="none"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+          ></path>
+        </svg>
+      )}{" "}
+      {children}
+    </button>
+  );
+}
+
+export default PrimaryButtonCOmponent;
