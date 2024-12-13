13e9eb3e22e02a18373376cab04b47b064e7183a
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-13 11:00:46 +0530

    Add utility function to capitalize first letter of words in a string

---
+++ src/utils/first-letter-capitalize.ts
@@ -0,0 +1,7 @@
+export default function firstLetterCapitalize(str: string) {
+  return str
+    .toLowerCase()
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
