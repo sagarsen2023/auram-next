9de95c14b5cdd9f862ae3d766ebf9ce10e04205a
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-13 23:41:15 +0530

    Add AddToCartSectionComponent and integrate breadcrumb navigation for improved product detail page experience

--- src/app/product-details/[id]/page.tsx
+++ src/app/product-details/[id]/page.tsx
@@ -1,21 +1,46 @@
 import { basePageClassNames } from "@/constants/universal-css";
 import React, { Suspense } from "react";
 import { fetchItemDetails } from "../utils";
-import DefaultLoaderComponent from "@/components/ui/default-loader.component";
 import ImageViewerComponent from "@/components/product-detail-components/image-viewer.component";
 import ItemDetailsAndPricing from "@/components/product-detail-components/item-details-and-pricing.component";
+import BreadCrumbComponent, {
+  BreadCrumbComponentProps,
+} from "@/components/ui/breadcrumb.component";
+import DefaultPageLoaderComponent from "@/components/ui/default-page-loader.component";
+import AddToCartSectionComponent from "@/components/product-detail-components/add-to-cart-section.component";
 
 async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const itemData = (await fetchItemDetails(id)).data;
   console.log("itemData", itemData);
+  const breadcrumbs: BreadCrumbComponentProps[] = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Products",
+      link: "/products",
+    },
+    {
+      name: itemData.itemName,
+    },
+  ];
   return (
-    <Suspense fallback={<DefaultLoaderComponent />}>
-      <div className={`${basePageClassNames} space-y-3`}>
-        {/* Component to view images */}
-        <ImageViewerComponent medias={itemData.itemMedia ?? []} />
-        {/* Item details and pricing */}
-        <ItemDetailsAndPricing item={itemData} />
+    <Suspense fallback={<DefaultPageLoaderComponent />}>
+      <div className={`${basePageClassNames}`}>
+        <BreadCrumbComponent breadCrumbItems={breadcrumbs} />
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-10">
+          {/* Component to view images */}
+          <ImageViewerComponent medias={itemData.itemMedia ?? []} />
+          <div className="space-y-4">
+            {/* Item details and pricing */}
+            <ItemDetailsAndPricing item={itemData} />
+            {/* Item Add to cart */}
+            {/* TODO: Send item data to manage cart */}
+            <AddToCartSectionComponent />
+          </div>
+        </div>
       </div>
     </Suspense>
   );
---
+++ src/components/product-detail-components/add-to-cart-section.component.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import PrimaryButtonCOmponent from "../buttons/primary-button.component";
+import { FaMinus, FaPlus } from "react-icons/fa6";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+
+function AddToCartSectionComponent() {
+  const maxQuantity = 10;
+  const [quantity, setQuantity] = useState(1);
+  return (
+    <div className="flex items-center gap-4 lg:w-1/2">
+      {/* Increment and decrement quantity part */}
+      <div className="flex justify-between w-36 gap-4 items-center p-1 border border-primary">
+        <PrimaryButtonCOmponent
+          disabled={quantity < 1}
+          onClick={() => setQuantity(quantity - 1)}
+        >
+          <FaMinus />
+        </PrimaryButtonCOmponent>
+        <span className="">{quantity}</span>
+        <PrimaryButtonCOmponent
+          disabled={quantity >= maxQuantity}
+          onClick={() => setQuantity(quantity + 1)}
+        >
+          <FaPlus />
+        </PrimaryButtonCOmponent>
+      </div>
+
+      {/* Add to cart button */}
+      <PrimaryButtonCOmponent>
+        <RiShoppingCart2Fill className="mr-4" />
+        Add to cart
+      </PrimaryButtonCOmponent>
+    </div>
+  );
+}
+
+export default AddToCartSectionComponent;
