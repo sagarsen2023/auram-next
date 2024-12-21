0f0e24e76036c91f4286bfd58e5a9dfd4d756505
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-21 14:59:05 +0530

    Add hover effect to SubNavComponent links for improved interactivity

--- src/components/navbar/sub-nav.component.tsx
+++ src/components/navbar/sub-nav.component.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 function SubNavComponent() {
+  const hoverEffect = "hover:border-b-2 border-secondary duration-300";
   return (
     <div className="flex justify-between items-center p-2">
       <div className="flex items-center gap-6 text-sm">
@@ -12,19 +13,19 @@ function SubNavComponent() {
       </div>
       <div className="flex items-center gap-6 text-sm">
         <Link href="/products">
-          <span>PRODUCTS</span>
+          <span className={hoverEffect}>PRODUCTS</span>
         </Link>
         <Link href="/collections">
-          <span>COLLECTIONS</span>
+          <span className={hoverEffect}>COLLECTIONS</span>
         </Link>
         <Link href="/bespoke">
-          <span>BESPOKE</span>
+          <span className={hoverEffect}>BESPOKE</span>
         </Link>
         <Link href="/about">
-          <span>ABOUT US</span>
+          <span className={hoverEffect}>ABOUT US</span>
         </Link>
         <Link href="/contact">
-          <span>CONTACT US</span>
+          <span className={hoverEffect}>CONTACT US</span>
         </Link>
       </div>
     </div>
