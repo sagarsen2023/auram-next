e0ffba2881a513c90174316bd69587c0dd459493
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-22 02:31:21 +0530

    Enhance registration schema validation with improved error messages and date types

--- src/validators/auth.schema.ts
+++ src/validators/auth.schema.ts
@@ -1,15 +1,19 @@
 import * as z from "zod";
 
 export const registrationSchema = z.object({
-  //   registrationId: z.string(),
-  email: z.string().email({ message: "Enter valid email" }),
-  phone: z.string().min(10, { message: "Enter valid mobile number" }),
-  honorific: z.enum(["Mr", "Mrs", "Ms"]),
-  fullName: z.string().min(3, { message: "Minimum 3 characters required" }),
-  whatsapp: z.string().optional(),
-  gender: z.enum(["male", "female", "other"]),
-  dob: z.string(),
-  dateOfAnniversary: z.string().optional(),
+    //   registrationId: z.string(),
+    email: z.string().email({ message: "Enter valid email" }),
+    phone: z.string().min(10, { message: "Enter valid mobile number" }),
+    honorific: z.enum(["Mr", "Mrs", "Ms"], {
+        message: "Honorific is required",
+    }),
+    fullName: z.string().min(3, { message: "Minimum 3 characters required" }),
+    whatsapp: z.string().optional(),
+    gender: z.enum(["male", "female", "other"], {
+        message: "Gender is required",
+    }),
+    dob: z.date(),
+    dateOfAnniversary: z.date().optional(),
 });
 
 export type RegistrationSchemaType = z.infer<typeof registrationSchema>;
