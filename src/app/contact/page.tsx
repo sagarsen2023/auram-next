4fb35959edbec73373dced74afbde612ed1e812f
sajaljana4 <sajaljana4@gmail.com>
2024-12-22 20:56:42 +0530

    gap

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ function Page() {
     <>
       <div className="max-w-6xl mx-auto pt-10 pb-4">
         <h1
-          className="relative py-10 text-center text-[34px] font-normal 
+          className="relative pt-2 pb-10 text-center text-[34px] font-normal 
            before:absolute before:bottom-[22px] before:left-[calc(50%-50px)] 
            before:block before:h-0.5 before:w-[80px] before:bg-black
            after:absolute after:bottom-[15px] after:right-[calc(50%-50px)] 
