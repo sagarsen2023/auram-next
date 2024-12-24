13adf91548c6e7020ab4ba930a7da25ecacefc68
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-24 22:44:38 +0530

    Update SidebarComponent to accept activeComponentKey prop for improved active state management

--- src/app/my/[[...slug]]/page.tsx
+++ src/app/my/[[...slug]]/page.tsx
@@ -18,7 +18,7 @@ const DashBoard = async ({
     <div className="w-full bg-gray-50">
       <div className="px-6 py-6 w-full lg:max-w-8xl mx-auto">
         <div className="flex min-w-screen flex-wrap gap-6 ">
-          <SidebarComponent />
+          <SidebarComponent activeComponentKey={slug ? slug[0] : "dashboard"} />
           <div className="flex-1 bg-white p-6 border border-gray-200 space-y-4">
             {(!slug || slug[0] === "dashboard") && <DashboardComponent />}
             {slug && slug[0] === "profile" && <MyProfile />}
