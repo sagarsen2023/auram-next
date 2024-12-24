4391950daec03e070ae776d0b8331ab4b7f9bd58
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-25 01:18:15 +0530

    Add authentication token handling to featured and new arrival components

--- src/components/home-components/new-arrival.component.tsx
+++ src/components/home-components/new-arrival.component.tsx
@@ -3,11 +3,13 @@ import { itemAPI } from "@/services/item.service";
 import React, { Suspense } from "react";
 import ProductCardComponent from "../cards/product-card.component";
 import DefaultLoaderComponent from "../ui/default-loader.component";
+import { getAuthToken } from "@/utils/cookie-store";
 
 async function NewArrivalComponent() {
+  const token = await getAuthToken();
   let items: ItemModel[] = [];
   try {
-    const response = await itemAPI.getLatestItems();
+    const response = await itemAPI.getLatestItems(token);
     items = response.data.slice(0, 4) ?? [];
   } catch (error) {
     console.log(error);
