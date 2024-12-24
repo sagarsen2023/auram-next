592957d7bb42723865b71e20fc3ebde0522e1dd8
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-24 22:30:58 +0530

    Implement dynamic routing for dashboard with slug-based components

---
+++ src/app/my/[[...slug]]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import SidebarComponent from "@/components/dashboard-components/sidebar/sidebar.component";
+import MyAddresses from "@/components/dashboard-components/address/addresses.component";
+import MyProfile from "@/components/dashboard-components/profile/profile.component";
+import DeleteAccount from "@/components/dashboard-components/delete-account/delete-account.component";
+import DashboardComponent from "@/components/dashboard-components";
+
+const DashBoard = async ({
+  params,
+}: {
+  params: Promise<{
+    slug?: ["profile" | "addresses" | "delete-account" | "dashboard" | ""];
+  }>;
+}) => {
+  const { slug } = await params;
+
+  return (
+    <div className="w-full bg-gray-50">
+      <div className="px-6 py-6 w-full lg:max-w-8xl mx-auto">
+        <div className="flex min-w-screen flex-wrap gap-6 ">
+          <SidebarComponent />
+          <div className="flex-1 bg-white p-6 border border-gray-200 space-y-4">
+            {(!slug || slug[0] === "dashboard") && <DashboardComponent />}
+            {slug && slug[0] === "profile" && <MyProfile />}
+            {slug && slug[0] === "addresses" && <MyAddresses />}
+            {slug && slug[0] === "delete-account" && <DeleteAccount />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoard;
