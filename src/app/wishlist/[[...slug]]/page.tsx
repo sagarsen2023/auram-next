47a52d366d4b041eee6c43abcf9a0b48c55f0b2b
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-25 17:51:41 +0530

    Sort filter applied for mobile devices

--- src/app/wishlist/[[...slug]]/page.tsx
+++ src/app/wishlist/[[...slug]]/page.tsx
@@ -1,12 +1,18 @@
+import FilterMenuComponent from "@/components/sort-filter-components/filter-menu.component";
+import SortByMenuComponent from "@/components/sort-filter-components/sort-by-menu.component";
 import BreadCrumbComponent, {
   BreadCrumbComponentProps,
 } from "@/components/ui/breadcrumb.component";
 import DefaultLoaderComponent from "@/components/ui/default-loader.component";
+// import { getAuthToken } from "@/utils/cookie-store";
+import { getItemParams, getSortFilterOptions } from "@/utils/sort-filter";
 import React, { Suspense } from "react";
 
 async function Page({ params }: { params: Promise<{ slug?: string[] }> }) {
   const slug = (await params).slug;
-  console.log("slug", slug);
+  const { sortOptions, filterOptions } = await getSortFilterOptions();
+  // const token = await getAuthToken();
+  const itemParams = getItemParams({ slug });
   const breadcrumbs: BreadCrumbComponentProps[] = [
     {
       name: "Home",
@@ -27,6 +33,23 @@ async function Page({ params }: { params: Promise<{ slug?: string[] }> }) {
       <div className="base-page">
         {/* BreadCrumb */}
         <BreadCrumbComponent breadCrumbItems={breadcrumbs} />
+        {/* Sort and filter Menu */}
+        <div className="flex justify-between lg:hidden items-center w-full my-3">
+          {filterOptions && filterOptions.length > 0 && (
+            <FilterMenuComponent
+              sortFor="/wishlist"
+              currentParams={itemParams}
+              filterOptions={filterOptions}
+            />
+          )}
+          {sortOptions && sortOptions.length > 0 && (
+            <SortByMenuComponent
+              sortFor="/wishlist"
+              currentParams={itemParams}
+              sortOptions={sortOptions[0].values}
+            />
+          )}
+        </div>
       </div>
     </Suspense>
   );
