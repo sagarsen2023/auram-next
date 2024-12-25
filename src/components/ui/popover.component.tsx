357d5a48ebc2c9696c750d1bcfdf1175bf5bd98c
sajaljana4 <sajaljana4@gmail.com>
2024-12-26 00:07:01 +0530

    popover edited

--- src/components/ui/popover.component.tsx
+++ src/components/ui/popover.component.tsx
@@ -30,7 +30,7 @@ const Popover = ({
   const popoverRef = useRef<HTMLDivElement>(null);
 
   const positionClass = {
-    top: "top-0 left-1/2 transform -translate-x-1/2 -translate-y-full",
+    top: "top-0 left-full transform -translate-x-1/2 -translate-y-full",
     bottom: "bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full",
     left: "left-0 top-1/2 transform -translate-y-1/2 -translate-x-full",
     right: "right-0 top-1/2 transform -translate-y-1/2 translate-x-full",
@@ -81,13 +81,13 @@ const Popover = ({
         <div
           className={`absolute shadow-lg ${positionClass[position]} ${sizeClass[size]} z-[999] `}
         >
-          <div className="rounded-md p-4 bg-gradient-to-br from-slate-100 to-white  shadow-lg border border-indigo-100  min-w-[280px] backdrop-blur-sm relative">
+          <div className="rounded-md p-4 bg-gradient-to-br from-slate-100 to-white  shadow-lg border border-indigo-100  min-w-[280px] backdrop-sm relative">
             <div className="mb-4">{content}</div>
 
             <div className="flex justify-end space-x-2">
               <button
                 className={classNameMerger([
-                  "px-4 py-2 bg-gray-100 text-gray-700 text-sm rounded-lg font-medium hover:bg-gray-100 transition-colors duration-200",
+                  "px-4 py-2 bg-gray-100 text-gray-700 text-sm rounded-lg font-medium hover:bg-gray-200 transition-colors duration-200",
                   cancelButtonClassName,
                 ])}
                 onClick={handleCancel}
@@ -96,7 +96,7 @@ const Popover = ({
               </button>
               <button
                 className={classNameMerger([
-                  "px-4 py-2 bg-gradient-to-r   from-yellow-500    via-yellow-600   to-yellow-700  hover:from-yellow-700   hover:via-yellow-800  hover:to-yellow-900 text-white text-sm rounded-lg font-medium hover:shadow-lg hover:shadow-indigo-100 transition-all duration-200",
+                  "px-4 py-2 bg-gradient-to-r   from-yellow-500    via-yellow-600   to-yellow-700  hover:from-yellow-600   hover:via-yellow-600  hover:to-yellow-600 text-white text-sm rounded-lg font-medium hover:shadow-lg hover:shadow-indigo-100 transition-all duration-200",
                   okButtonClassName,
                 ])}
                 onClick={handleOk}
