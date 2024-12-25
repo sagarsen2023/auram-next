8242da3552eca33f2a2075e993d02d321062d729
sajaljana4 <sajaljana4@gmail.com>
2024-12-26 00:40:17 +0530

    model optional mentioned

--- src/models/addresses/address.model.tsx
+++ src/models/addresses/address.model.tsx
@@ -2,23 +2,23 @@ import BaseApiResponse from "../common/base-api-response.model";
 import UserResponse from "../common/user-response.model";
 
 export interface AddressesModel {
-  _id: string;
+  _id?: string;
   line1: string;
-  line2: string;
+  line2?: string;
   city: string;
   state: string;
   country: string;
   pincode: string;
-  isDefault: boolean;
-  status: boolean;
-  isDeleted: boolean;
+  isDefault?: boolean;
+  status?: boolean;
+  isDeleted?: boolean;
   countryCode: string;
   phone: string;
   name: string;
-  createdBy: UserResponse;
+  createdBy?: UserResponse;
   updatedBy?: UserResponse;
-  createdAt: string;
-  updatedAt: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface AllAddressApiResponse extends BaseApiResponse {
