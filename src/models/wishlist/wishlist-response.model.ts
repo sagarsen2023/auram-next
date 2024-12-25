b4136060b62c891c79a38c5aedb3ae2370889319
Sagar Sen <sagarsen.dev@gmail.com>
2024-12-25 19:02:19 +0530

    Add WishlistModel and WishlistItemResponse interfaces to enhance wishlist response structure

--- src/models/wishlist/wishlist-response.model.ts
+++ src/models/wishlist/wishlist-response.model.ts
@@ -1,4 +1,6 @@
 import BaseApiResponse from "../common/base-api-response.model";
+import CustomerModel from "../common/customer-model";
+import { ItemModel } from "../product-category-collections/item.model";
 
 export interface AddToWishlistResponse extends BaseApiResponse {
   data: {
@@ -9,4 +11,18 @@ export interface AddToWishlistResponse extends BaseApiResponse {
     createdAt: string;
     updatedAt: string;
   };
+}
+
+export interface WishlistModel {
+  _id: string;
+  user: CustomerModel;
+  item: ItemModel;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface WishlistItemResponse extends BaseApiResponse {
+  totalCount: number;
+  data: WishlistModel[];
 }
