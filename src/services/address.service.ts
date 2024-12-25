9408402793df636387293205b92008813cb72c66
sajaljana4 <sajaljana4@gmail.com>
2024-12-26 00:07:16 +0530

    address service for get

--- src/services/address.service.ts
+++ src/services/address.service.ts
@@ -1,9 +1,15 @@
 import { fetchAPI } from "./config";
-import AllAddressApiResponse from "@/models/addresses/address.model";
+import { AllAddressApiResponse } from "@/models/addresses/address.model";
 import { MY_ADDRESSES_URL } from "./queryUrls";
+import { getAuthToken } from "@/utils/token-store";
 
 const addressesAPI = {
-  fetchAllAddresses: async () =>
-    fetchAPI.get<AllAddressApiResponse>(MY_ADDRESSES_URL),
+  fetchAllAddresses: async () => {
+    return await fetchAPI.get<AllAddressApiResponse>(MY_ADDRESSES_URL, {
+      headers: {
+        authorization: `Bearer ${getAuthToken()}`,
+      },
+    });
+  },
 };
 export default addressesAPI;
